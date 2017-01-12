d9df031dd88e2301eb0645ff8c4e897c11dfa073
LectorRSS <the.nightmare.c@gmail.com>
2017-01-12 17:13:49 -0300

    Version Beta

--- main.js
+++ main.js
@@ -13,7 +13,49 @@ $( ".clientname1" ).keyup(function() {
 $( ".devurl" ).keyup(function() {
    var mail = $(this).val()
    $(".devurl_in").html("<a href='http://developers.bigbuda.cl/" + mail + "' >http://developers.bigbuda.cl/" + mail + "</a>");
+
 })
+
+$( ".wp_login" ).keyup(function() {
+   var mail = $(this).val()
+   $(".wp_login_in").html(mail);
+})
+
+$( ".wp_pass" ).keyup(function() {
+   var mail = $(this).val()
+   $(".wp_pass_in").html(mail);
+})
+
+$( ".wp_admin" ).keyup(function() {
+   var mail = $(this).val()
+   $(".wp_admin_in").html("<a href='http://" + mail + "' >http://" + mail + "</a>");
+})
+
+$( ".backup" ).keyup(function() {
+   var mail = $(this).val()
+   $(".backup_in").html("<a href='" + mail + "' >" + mail + "</a>");
+})
+
+$( ".tutorial" ).keyup(function() {
+   var mail = $(this).val()
+   $(".tutorial_in").html("<a href='" + mail + "' >" + mail + "</a>");
+})
+
+$( ".ana_admin" ).keyup(function() {
+   var mail = $(this).val()
+   $(".wp_admin_in").html("<a href='" + mail + "' >" + mail + "</a>");
+})
+
+$( ".ana_user" ).keyup(function() {
+   var mail = $(this).val()
+   $(".ana_user_in").html("<a href='" + mail + "' >" + mail + "</a>");
+})
+
+$( ".ana_pass" ).keyup(function() {
+   var mail = $(this).val()
+   $(".ana_pass_in").html("<a href='" + mail + "' >" + mail + "</a>");
+})
+
 
 
 $(".dev_start_btn").on('click', function(event) {
